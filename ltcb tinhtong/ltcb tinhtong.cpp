da12d01cd52b901f143c5aa00c705a3c11c2f12a
Shinobeee <lelinh88999@gmail.com>
2024-02-26 08:54:29 +0700

    Add project files.

---
+++ ltcb tinhtong/ltcb tinhtong.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int so1, so2, tong;
+    cout << "CHUONG TRINH TINH TONG\n";
+    cout << "nhap so thu nhat:";
+    cin >> so1;
+    cout << "nhap so thu hai:";
+    cin >> so2;
+    tong = so1 + so2;
+    cout << so1 << "+" << so2 << "=" << tong << endl;
+    return 0;
+}
